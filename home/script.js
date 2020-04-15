1f8f92fb1e5b379aa870d77b7e9435e2982bf2d8
flaviosilva <flavioandre.2001.9.7@gmail.com>
2020-04-15 15:02:36 +0100

    Add new page terminal
    Página inicial de terminal

---
+++ home/script.js
@@ -0,0 +1,12 @@
+const ProjectsList = document.getElementsByClassName("ProjectsList");
+
+
+main();
+
+function main() {
+    ProjectsList[0].style.display = "block";
+}
+
+function ProjectsChange() {
+
+}
