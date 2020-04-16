37575922718ca7ecdead4b043752809735631a59
flaviosilva <flavioandre.2001.9.7@gmail.com>
2020-04-16 20:39:41 +0100

    new nav e org. files and code

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const DotsGL = document.getElementById("DotsGL");
 const RedirectingTime = document.getElementById("RedirectingTime");
 
 let counter = 0;
+let RedirectingCounter = 0;
+let timer = setInterval(LinesShow, 500);;
 
 main()
 
@@ -15,9 +17,8 @@ function main() {
     DataLogin.innerHTML = new Date().getDate() + "/" + new Date().getMonth() + "/" + new Date().getFullYear() + " - ";
     linha[0].style.visibility = "visible";
     linha[1].style.visibility = "visible";
+    LinesShow();
 }
-
-const timer = setInterval(LinesShow, 500);
 
 function LinesShow() {
     counter++;
@@ -57,6 +58,7 @@ function LinesShow() {
             DotsGL.innerHTML += ".";
             break;
         case 13:
+            DotsGL.innerHTML += "./";
             answer[3].style.visibility = "visible";
             break;
         case 14:
@@ -65,26 +67,46 @@ function LinesShow() {
             break;
         case 15:
         case 16:
-            RedirectingTime.innerHTML = "4";
+            RedirectingTime.innerHTML = "9";
             break;
         case 17:
         case 18:
-            RedirectingTime.innerHTML = "3";
+            RedirectingTime.innerHTML = "8";
             break;
         case 19:
         case 20:
-            RedirectingTime.innerHTML = "2";
+            RedirectingTime.innerHTML = "7";
             break;
         case 21:
         case 22:
-            RedirectingTime.innerHTML = "1";
+            RedirectingTime.innerHTML = "6";
             break;
         case 23:
         case 24:
+            RedirectingTime.innerHTML = "5";
+            break;
+        case 25:
+        case 26:
+            RedirectingTime.innerHTML = "4";
+            break;
+        case 27:
+        case 28:
+            RedirectingTime.innerHTML = "3";
+            break;
+        case 29:
+        case 30:
+            RedirectingTime.innerHTML = "2";
+            break;
+        case 31:
+        case 32:
+            RedirectingTime.innerHTML = "1";
+            break;
+        case 33:
+        case 34:
             RedirectingTime.innerHTML = "0";
             break;
         default:
-            clearInterval(timer);
+            // clearInterval(timer);
             // location.replace("./Pages/home/home.html");
             break;
     }
