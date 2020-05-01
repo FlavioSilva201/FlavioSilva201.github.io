b226549413def5c00ca4bd112ccfb72119341013
flaviosilva <flavioandre.2001.9.7@gmail.com>
2020-05-01 21:06:27 +0100

    Completion of the Skill page
    Conclusão da pagina Skill

--- Pages/Skills/script.js
+++ Pages/Skills/script.js
@@ -1,12 +1,48 @@
 // Tab Separadores
 const tabTitlesDescription = document.getElementById("tabTitlesDescription");
 const tabTitlesSN = document.getElementById("tabTitlesSN");
-const NavBar = document.getElementsByClassName("NavBar");
 
 // Conteudos, descrição ou Links
 const tabConteudoDescription = document.getElementById("tabConteudoDescription");
 const tabConteudoSN = document.getElementById("tabConteudoSN");
 
+tabTitlesDescription.addEventListener("click", () => {
+    tabTitlesDescription.classList.add("SelectedTab");
+    tabTitlesSN.classList.remove("SelectedTab");
+    tabConteudoDescription.style.display = "block";
+    tabConteudoSN.style.display = "none";
+});
+
+tabTitlesSN.addEventListener("click", () => {
+    tabTitlesSN.classList.add("SelectedTab");
+    tabTitlesDescription.classList.remove("SelectedTab");
+    tabConteudoSN.style.display = "flex";
+    tabConteudoDescription.style.display = "none";
+});
+
+
 // Btns
 const SNLeft = document.getElementById("SNLeft");
 const SNRight = document.getElementById("SNRight");
+
+const SocNets = document.getElementsByClassName("SocNets"); // links
+let SocNetsSelected = 0;
+
+
+SNLeft.addEventListener("click", () => {
+    SocNets[SocNetsSelected].classList.remove("SocialSelected");
+    SocNetsSelected--;
+    if (SocNetsSelected < 0) {
+        SocNetsSelected = SocNets.length - 1;
+    }
+    SocNets[SocNetsSelected].classList.add("SocialSelected");
+});
+
+SNRight.addEventListener("click", () => {
+    SocNets[SocNetsSelected].classList.remove("SocialSelected");
+    SocNetsSelected++;
+    if (SocNetsSelected > SocNets.length - 1) {
+        SocNetsSelected = 0;
+    }
+    SocNets[SocNetsSelected].classList.add("SocialSelected");
+});
