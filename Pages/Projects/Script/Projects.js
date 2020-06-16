0ee12ae9809ace11704a6bf282038525d5c01d77
flaviosilva <flavioandre.2001.9.7@gmail.com>
2020-06-16 23:48:33 +0100

    Page Projects

---
+++ Pages/Projects/Script/Projects.js
@@ -0,0 +1,72 @@
+export default {
+  SitesPorreiros: {
+    name: "Sites Porreiros",
+    img: {
+      src: "https://i.ibb.co/jJTYHg8/Sites-Porreiros.png",
+      alt: "Sites Porreiros",
+    },
+    decription: {
+      ing: `Sites Porreiros is a site where I publish simple scripts, games, apps, websites, funny mini projects. \n
+      The real purpose is simply to entertain when there is nothing to do.`,
+      pt: `Sites Porreiros é um site onde publico scripts simples, jogos, aplicativos, sites, mini projetos engraçados. \n
+      O verdadeiro objetivo é simplesmente entreter quando não há nada a fazer.`,
+    },
+    webSite: "https://sitesporreiros.netlify.app/",
+    repository: "https://github.com/201flaviosilva/SitesPorreiros",
+  },
+  VanillaJavaScript: {
+    name: "Vanilla JavaScript",
+    img: {
+      src: "https://i.ibb.co/Tm3SLTP/Vanilla-JS.png",
+      alt: "Vanilla JavaScript",
+    },
+    decription: {
+      ing: `VanillaJavaScript`,
+      pt: `VanillaJavaScript`,
+    },
+    webSite: "https://vanillajavascript.netlify.app/",
+    repository: "https://github.com/201flaviosilva/VanillaJavaScript",
+  },
+  InvasoresDoEspaco: {
+    name: "Invasores Do Espaco",
+    img: {
+      src: "#",
+      alt: "Invasores Do Espaco",
+    },
+    decription: {
+      ing: `This is a game inspired by the 1978 original "Space Invaders" by Taito. \n
+      This game was written in Visual Basic 2010, with the objective of being one of the many game on my secondary final project, where I created some game modes, like "Team Deathmatch" for 2 or 4 players.Another game mode is "Points" where up to 6 players must defeat the invaders and the one with the most points wins.`,
+      pt: `Este é um jogo inspirado no original de 1978 "Space Invaders" por Taito. \n
+      Este jogo foi escrito em Visual Basic 2010, com o objetivo de ser um dos jogos de um projeto final do secundário. \n
+      Onde criei uns modos de jogo, como "Team Deathmatch" de 2 ou 4 jogadores, outro modo de jogo é "Pontos" onde até 6 jogadores tem de derrotar os invasores e o que tiver mais pontos vence.`,
+    },
+    webSite: "#",
+    repository: "https://github.com/201flaviosilva/InvasoresDoEspaco",
+  },
+  CarteiraOnline: {
+    name: "Carteira Online",
+    img: {
+      src: "#",
+      alt: "Carteira Online",
+    },
+    decription: {
+      ing: `CarteiraOnline`,
+      pt: `CarteiraOnline`,
+    },
+    webSite: "http://carteiraonline.epizy.com/",
+    repository: "https://github.com/201flaviosilva/CarteiraOnline",
+  },
+  CSSFreeStyle: {
+    name: "CSS Free Style",
+    img: {
+      src: "#",
+      alt: "CSS Free Style",
+    },
+    decription: {
+      ing: `CSSFreeStyle`,
+      pt: `CSSFreeStyle`,
+    },
+    webSite: "#",
+    repository: "https://github.com/201flaviosilva/CSSFreeStyle",
+  },
+};
