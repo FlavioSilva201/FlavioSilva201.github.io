c44ffd6e41f97bdc7048ca0147c46b884d709c27
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2020-10-07 21:02:02 +0100

    Multi language Game

---
+++ Pages/Games/script.js
@@ -0,0 +1,22 @@
+main();
+function main() {
+	let indHistorySel;
+	if (localStorage.getItem("LocalLang") === "English") indHistorySel = "EN";
+	else indHistorySel = "PT";
+	getData(indHistorySel);
+}
+
+async function getData(indHistorySel) {
+	await fetch(`../../Languages/Data/Game/${indHistorySel}.json`)
+		.then(response => response.json())
+		.then(data => indHistorySel = data);
+	changeDom(indHistorySel);
+}
+
+function changeDom(indHistorySel) {
+	document.getElementById("Magito").innerHTML = indHistorySel.Magito;
+	document.getElementById("TicTacToe").innerHTML = indHistorySel.TicTacToe;
+	document.getElementById("SitesPorreiros").innerHTML = indHistorySel.SitesPorreiros;
+	document.getElementById("InvasoresDoEspaco").innerHTML = indHistorySel.InvasoresDoEspaco;
+	document.getElementById("PlanetaGames").innerHTML = indHistorySel.PlanetaGames;
+}
