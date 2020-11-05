eb672a43502ecd48275cec736baae8f5bb87c814
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2020-11-05 23:57:35 +0000

    Delete all lines of v1 and start ball games and main menu

--- Pages/Index/script.js
+++ Pages/Index/script.js
@@ -1,119 +1,59 @@
-const DataLogin = document.getElementById("DataLogin");
-const linha = document.getElementsByClassName("linha");
-const commandRight = document.getElementsByClassName("commandRight");
-const answerUser = document.getElementsByClassName("answerUser");
-const answer = document.getElementsByClassName("answer");
+import { randomNumber, randomColor } from "../../utils.js";
 
-const DotsGL = document.getElementById("DotsGL");
-const RedirectingTime = document.getElementById("RedirectingTime");
+const canvas = document.getElementById("canvas");
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight;
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+var ctx = canvas.getContext("2d");
 
-let counter = 0;
-let RedirectingCounter = 0;
-let timer = setInterval(LinesShow, 500);
+const balls = [];
+let score = 0;
+let x = 0;
+let y = 0;
+document.addEventListener("mousemove", (event) => {
+	x = event.clientX;
+	y = event.clientY;
+});
 
-main();
-
-function main() {
-  const date = new Date();
-  DataLogin.innerHTML =
-    date.getDate() +
-    "/" +
-    (date.getMonth() + 1) +
-    "/" +
-    date.getFullYear() +
-    " - ";
-  linha[0].style.visibility = "visible";
-  linha[1].style.visibility = "visible";
-  LinesShow();
+class Ball {
+	constructor() {
+		const x = randomNumber(0, canvasWidth);
+		this.size = randomNumber(5, 25);
+		this.x = x;
+		this.y = canvasHeight + this.size;
+		this.startX = x;
+		this.speedX = randomNumber(-1, 1);
+		this.color = "rgb(" + randomColor() + "," + randomColor() + "," + randomColor() + ")";
+	}
 }
 
-function LinesShow() {
-  counter++;
-  switch (counter) {
-    case 1:
-      commandRight[0].style.visibility = "visible";
-      break;
-    case 2:
-      answerUser[0].style.visibility = "visible";
-      linha[2].style.visibility = "visible";
-      linha[3].style.visibility = "visible";
-      break;
-    case 3:
-      commandRight[1].style.visibility = "visible";
-      break;
-    case 4:
-      answerUser[1].style.visibility = "visible";
-      break;
-    case 5:
-      answer[0].style.visibility = "visible";
-      break;
-    case 6:
-      answerUser[2].style.visibility = "visible";
-      break;
-    case 7:
-      answer[1].style.visibility = "visible";
-      break;
-    case 8:
-      answer[2].style.visibility = "visible";
-      break;
-    case 9:
-    case 10:
-    case 11:
-    case 12:
-      DotsGL.style.visibility = "visible";
-      DotsGL.innerHTML += ".";
-      break;
-    case 13:
-      DotsGL.innerHTML += "./";
-      answer[3].style.visibility = "visible";
-      break;
-    case 14:
-      answer[4].style.visibility = "visible";
-      RedirectingTime.style.visibility = "visible";
-      break;
-    case 15:
-    case 16:
-      RedirectingTime.innerHTML = "9";
-      break;
-    case 17:
-    case 18:
-      RedirectingTime.innerHTML = "8";
-      break;
-    case 19:
-    case 20:
-      RedirectingTime.innerHTML = "7";
-      break;
-    case 21:
-    case 22:
-      RedirectingTime.innerHTML = "6";
-      break;
-    case 23:
-    case 24:
-      RedirectingTime.innerHTML = "5";
-      break;
-    case 25:
-    case 26:
-      RedirectingTime.innerHTML = "4";
-      break;
-    case 27:
-    case 28:
-      RedirectingTime.innerHTML = "3";
-      break;
-    case 29:
-    case 30:
-      RedirectingTime.innerHTML = "2";
-      break;
-    case 31:
-    case 32:
-      RedirectingTime.innerHTML = "1";
-      break;
-    case 33:
-    case 34:
-      RedirectingTime.innerHTML = "0";
-      break;
-    default:
-      clearInterval(timer);
-      window.location = "./Pages/Home/index.html";
-      break;
-  }
+setInterval(() => balls.push(new Ball()), 250);
+setInterval(() => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	balls.forEach((b, index) => {
+		b.y--;
+		if (b.y + b.size < 0) balls.splice(index, 1);
+		b.x += b.speedX;
+		if ((b.startX - 50 > b.x) || (b.startX + 50 < b.x)) b.speedX *= -1;
+		collisionDetection(b, index);
+		ctx.beginPath();
+		ctx.fillStyle = b.color;
+		ctx.arc(b.x, b.y, b.size, 0, Math.PI * 2, true);
+		ctx.fill();
+	});
+}, 10);
+
+function collisionDetection(b, index) {
+	if (x > b.x - b.size && x < b.x + b.size &&
+		y > b.y - b.size && y < b.y + b.size) {
+		balls.splice(index, 1);
+		balls.push(new Ball());
+		updateScore();
+	}
+}
+
+function updateScore() {
+	score++;
+	document.getElementById("score").innerHTML = score;
 }
