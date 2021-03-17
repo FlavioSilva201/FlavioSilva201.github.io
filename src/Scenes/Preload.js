d2eeaa07682214aa26ab1d1585a5441b2a7f7930
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2021-03-17 00:04:55 +0000

    Organize Sprites and add Option Scene sprites

--- src/Scenes/Preload.js
+++ src/Scenes/Preload.js
@@ -1,9 +1,16 @@
 import ProgressBar from "../Components/ProgressBar";
 
-// ---- Assets
-import Player from "../Assets/Sprites/Player.png";
-import Sprite from "../Assets/Sprites/Sprite.png";
-import Grass from "../Assets/Sprites/Grass.png";
+// -------- Imgs --------
+// ---- Sprites ----
+import Player from "../Assets/Img/Sprites/Player.png";
+import Sprite from "../Assets/Img/Sprites/Sprite.png";
+import Grass from "../Assets/Img/Sprites/Grass.png";
+
+// ---- UI ----
+// -- Options --
+import Flags from "../Assets/Img/UI/Options/Flags/Flags.png";
+import FullScreen from "../Assets/Img/UI/Options/FullScreen/FullScreen.png";
+import Sound from "../Assets/Img/UI/Options/Sound/Sound.png";
 
 export default class Preload extends Phaser.Scene {
 	constructor() {
@@ -14,6 +21,7 @@ export default class Preload extends Phaser.Scene {
 		const progressBar = new ProgressBar(this);
 
 		this.importSprites();
+		this.importUI();
 		this.importSounds();
 
 		this.load.on('progress', (p) => progressBar.updateBar(p));
@@ -22,6 +30,8 @@ export default class Preload extends Phaser.Scene {
 		this.load.on('complete', () => {
 			progressBar.complete();
 			this.scene.start("Home");
+			// this.scene.start("Start");
+			// this.scene.start("Options");
 		});
 	}
 
@@ -29,6 +39,12 @@ export default class Preload extends Phaser.Scene {
 		this.load.image("Sprite", Sprite);
 		this.load.image("Player", Player);
 		this.load.image("Grass", Grass);
+	}
+
+	importUI() {
+		this.load.spritesheet("Flags", Flags, { frameWidth: 32, frameHeight: 32 });
+		this.load.spritesheet("FullScreen", FullScreen, { frameWidth: 32, frameHeight: 32 });
+		this.load.spritesheet("Sound", Sound, { frameWidth: 32, frameHeight: 32 });
 	}
 
 	importSounds() { }
